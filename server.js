782be143869dc815d5103a83e7987a3b5326410f
Tony Bavalan <tonybavalan@wowtruck.in>
2022-03-01 22:05:07 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+// Imports
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+// Static Files
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
+app.set('views', path.join(__dirname, 'public/views'))
+app.set('view engine', 'ejs');
+
+// Navigation
+app.get('/', (req, res) => {
+  res.render('index')
+})
+app.get('/projects', (req, res) => {
+  res.render('frontend-bootcamp')
+})
+app.post('/sendmsg', (req, res) => {
+  res.render('frontend-bootcamp')
+})
+app.listen(port, () => {
+    console.log(`app listening at http://localhost:${port}`)
+})
